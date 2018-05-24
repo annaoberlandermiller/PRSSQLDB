5ad2a11275a345e4e3b786086ce9db96b025daca
Anna Miller <annaoberlandermiller@gmail.com>
2018-05-24 13:23:44 -0400

    adding User table to GitHub

--- User.sql
+++ User.sql
@@ -1,16 +1,26 @@
-create database [User]
-
+use master; -- select master db.  ensures you're deleting what you're actually wanting to delete
+go --ensures no rollback
+drop database if exists prssql; --
+go
+create database prssql; --create database called prssql
+go
+use prssql;
+go 
 create table [User] (
 
-	Id int primary key default (1,1) not null,
-	Username nvarchar(30) not null unique,
+	Id int identity (1, 1) primary key not null,
+	Username nvarchar(30) not null,
 	Password nvarchar(30) not null,
 	Firstname nvarchar(30) not null,
 	Lastname nvarchar(30) not null,
 	Phone nvarchar(12) not null,
 	Email nvarchar(255) not null,
-	IsReviewer bit,
-	IsAdmin bit,
-	Active bit
+	IsReviewer bit not null default 0,
+	IsAdmin bit not null default 0,
+	Active bit not null default 1
 
 );
+
+go 
+create unique index uix_username on [User](Username);  --Create a unique index for the column username on table user. This is another way of making sure that usernames are not duplicated. 
+go -- uix_username is the name of the index
